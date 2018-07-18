f860eb34f7e128def375f0222fe3364ed114a0f3
Bestun Gardi <bestun95@yahoo.com>
2018-07-18 00:24:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your code in this file!
+
+function scuberGreetingForFeet(num) {
+  let answer;
+  if(num<= 400)
+
+  {
+    return "This one is on me!";
+  }
+  else if (num >= 2000)
+  
+    {
+      answer = "I will gladly take your thirty bucks.";
+      return answer;
+      
+    }
+  
+}
+
+function ternaryCheckCity(city) {
+  city===NYC ? "Ok, sounds good." : "No go.";
+}
